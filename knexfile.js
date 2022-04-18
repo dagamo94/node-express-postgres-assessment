fc85bfeef5433610a613758c457eba984437de2d
dan <da.ga.mo.94@gmail.com>
2022-04-18 09:59:26 -0600

    initial commit - project setup, db created and seeded

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+if (process.env.USER) require("dotenv").config();
+
+const path = require("path");
+
+// default DB_URL: postgresql://postgres@localhost/postgres
+const {
+  DATABASE_URL = "postgres://uoekwdiv:DoUOpMHR5dBUKi7GfZ5AWn981394Ww8m@heffalump.db.elephantsql.com/uoekwdiv",
+} = process.env;
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
+};
